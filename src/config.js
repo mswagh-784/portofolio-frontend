8a5c8201fc01a5bc1a2568f4bda6cd5fdf485e18
Hitesh2004-bhosale <bhosalehitech7722@gmail.com>
2025-05-22 17:59:01 +0530

    added backend url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   apiUrl:
     import.meta.env.MODE === 'production'
-      ? 'https://portfolio-backend-sage-seven.vercel.app/' // Updated to your deployed backend URL
+      ? 'https://portfolio-backend-ai05.onrender.com' // Updated to your deployed backend URL
       : 'http://localhost:5000'
 };
 
